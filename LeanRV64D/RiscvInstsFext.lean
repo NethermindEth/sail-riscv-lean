3a1aa213875bc4328f55b945a6a0e6b5cc4c04d6
tobiasgrosser <tobiasgrosser@users.noreply.github.com>
2025-06-20 18:55:34 +0000

    Update main to output generated at 3173bbe

--- LeanRV64D/RiscvInstsFext.lean
+++ LeanRV64D/RiscvInstsFext.lean
@@ -371,7 +371,7 @@ def feq_quiet_S (v1 : (BitVec 32)) (v2 : (BitVec 32)) : (Bool × (BitVec 5)) :=
     else (zeros (n := 5))
   (result, fflags)
 
-/-- Type quantifiers: k_ex378099# : Bool -/
+/-- Type quantifiers: k_ex378120# : Bool -/
 def flt_S (v1 : (BitVec 32)) (v2 : (BitVec 32)) (is_quiet : Bool) : (Bool × (BitVec 5)) :=
   let (s1, e1, m1) := (fsplit_S v1)
   let (s2, e2, m2) := (fsplit_S v2)
@@ -403,7 +403,7 @@ def flt_S (v1 : (BitVec 32)) (v2 : (BitVec 32)) (is_quiet : Bool) : (Bool × (Bi
       else (zeros (n := 5)))
   (result, fflags)
 
-/-- Type quantifiers: k_ex378167# : Bool -/
+/-- Type quantifiers: k_ex378188# : Bool -/
 def fle_S (v1 : (BitVec 32)) (v2 : (BitVec 32)) (is_quiet : Bool) : (Bool × (BitVec 5)) :=
   let (s1, e1, m1) := (fsplit_S v1)
   let (s2, e2, m2) := (fsplit_S v2)
